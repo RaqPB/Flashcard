9d0cbebc54ea6f744e3b7f8e10fc4768eb56ad67
RaqPB <142899720+RaqPB@users.noreply.github.com>
2025-07-13 16:26:38 -0300

    Add files via upload

---
+++ src/js/homepageScripts.js
@@ -0,0 +1,10 @@
+let create = document.querySelector(".create");
+let play = document.querySelector(".play");
+
+create.addEventListener("click", () => {
+    window.location.href = "createCard.html";
+});
+
+play.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
