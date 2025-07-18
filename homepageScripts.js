e5ef0ee06ce73281f04785b606e409a4a1d1afad
RaqPB <142899720+RaqPB@users.noreply.github.com>
2025-07-18 19:06:39 -0300

    Add files via upload

---
+++ homepageScripts.js
@@ -0,0 +1,10 @@
+let hiragana = document.querySelector(".hiragana");
+let katakana = document.querySelector(".katakana");
+
+hiragana.addEventListener("click", () => {
+    window.location.href = "hiragana.html";
+});
+
+katakana.addEventListener("click", () => {
+    window.location.href = "katakana.html";
+});
